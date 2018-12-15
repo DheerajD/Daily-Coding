1fa556df1a50fb74e55702c99515782bca660102
DheerajD <dheeraj.doodhya@gmail.com>
2018-12-15 23:50:00 +0530

    Check if each Matrix row rotation of each other

---
+++ Matrix questions/p7 check if all rows of a matrix are circular rotation of each other/CheckMatrixRowRotation.java
@@ -0,0 +1,10 @@
+//To check if all rows are circular rotation of each other. 
+//To solve this, I'll create a string out of the first row and then would concatenate it to itself. This string I would use for reference. I'll compare all other row strings with this to check if they are substring of this string or not.
+
+public class CheckMatrixRowRotation {
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
