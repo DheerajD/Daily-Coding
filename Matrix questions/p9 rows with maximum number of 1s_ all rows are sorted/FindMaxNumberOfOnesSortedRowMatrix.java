7ef132b7f4541116f1324007c109441b83fe5eea
DheerajD <dheeraj.doodhya@gmail.com>
2018-12-19 00:19:53 +0530

    Find row with max number of 1s

--- Matrix questions/p9 rows with maximum number of 1s_ all rows are sorted/FindMaxNumberOfOnesSortedRowMatrix.java
+++ Matrix questions/p9 rows with maximum number of 1s_ all rows are sorted/FindMaxNumberOfOnesSortedRowMatrix.java
@@ -4,7 +4,10 @@
 
 public class FindMaxNumberOfOnesSortedRowMatrix {
 	public static void main(String args[]) {
-		int[][] a = {{0,0,1,1},{0,0,0,1},{1,1,1,1}};
+		int[][] a = { { 0, 0, 0, 1 }, 
+                        { 0, 1, 1, 1 }, 
+                        { 1, 1, 1, 1 }, 
+                        { 0, 0, 0, 0 } };
 		int max1s = getMax1s(a);
 		System.out.println("Row with max number of 1s is : "+max1s);
 	}
@@ -15,8 +18,27 @@ public class FindMaxNumberOfOnesSortedRowMatrix {
 		int max = Integer.MIN_VALUE;
 		
 		for(int i=0; i<n; i++) {
-		
+			int l = getFirstindex1(a[i], 0 , m-1);
+			if(l== -999)
+				continue;
+			int diff = m-l;
+			if(max < diff)
+				max = diff;
 		}
 		return max;
+	}
+	
+	public static int getFirstindex1(int[] a, int l, int h) {
+		int mid;
+		if(l<=h) {
+			mid = (l+h)/2;
+			if((mid==0 || a[mid-1] == 0) && a[mid]==1)
+				return mid;	
+			else if(a[mid]==0)
+				return getFirstindex1(a, mid+1, h);
+			else
+				return getFirstindex1(a, l, mid-1);
+		}
+		return -999;
 	}
 }
---
+++ Matrix questions/p9 rows with maximum number of 1s_ all rows are sorted/FindMaxNumberOfOnesSortedRowMatrix2.java
@@ -0,0 +1,31 @@
+//approach third :  here we will find first occurrence of 1 in first row of matrix and will move down the matrix, check for leftmost 1.If leftmost one lies on left side of existing pinter, change pointer, else ignore
+//Dheeraj Doodhya
+
+public class FindMaxNumberOfOnesSortedRowMatrix2 {
+	public static void main(String args[]) {
+		int[][] a = { { 0, 0, 0, 1 }, 
+                        { 0, 1, 1, 1 }, 
+                        { 0, 0, 1, 1 }, 
+                        { 0, 0, 0, 0 } };
+						
+		int max1 = getRowMax1(a);
+		System.out.println("max number of 1 in any row is :" +max1);
+	}
+	
+	public static int getRowMax1(int[][] a) {
+		int n = a.length;
+		int m = a[0].length;
+		
+		int ptr=m-1;
+		
+		for(int i=0; i<n; i++) {
+			while(a[i][ptr] == 1){ 
+				ptr--;
+			
+			if(ptr == -1)
+				return m;
+			}
+		}
+		return m-1-ptr;
+	}
+}
